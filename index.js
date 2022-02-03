344c0d13f3a6a16b18fb34e5d4edb18608cbce14
alexander gubernikoff <agubernikoff@gmail.com>
2022-02-03 18:43:01 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(function (x) {
+    return x.toLowerCase() == string.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers, letters) {
+  const newArr = drivers.filter((x) => x.startsWith(letters));
+  return newArr;
+}
+function matchName(drivers, str) {
+  return drivers.filter((x) => x.name === str);
+}
